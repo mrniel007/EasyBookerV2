41e21171646aef36b7f2d8b3e5c832841c193880
mrniel007 <niel.hvg@gmail.com>
2020-03-26 21:16:37 -0400

    Modificaciones de estilos de CSS y del Login

---
+++ src/app/places/offers/offers-item/offers-item.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { OffersItemComponent } from './offers-item.component';
+
+describe('OffersItemComponent', () => {
+  let component: OffersItemComponent;
+  let fixture: ComponentFixture<OffersItemComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ OffersItemComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(OffersItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
